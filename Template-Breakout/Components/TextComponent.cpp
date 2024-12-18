c42bf4dc219a3b7c63a18838598a04de0e223c5b
Loris Tessier <loris.tessier34@orange.fr>
2024-12-18 15:41:53 +0100

    TextComponent a changer

--- Template-Breakout/Components/TextComponent.cpp
+++ Template-Breakout/Components/TextComponent.cpp
@@ -1,1 +1,39 @@
 #include "TextComponent.h"
+
+#include <iostream>
+#include <string>
+#include "../Services/GameManager.h"
+
+#include "../resources.h"
+
+
+TextComponent::TextComponent() 
+{
+	font = sf::Font(DEFAULT_FONT_PATH);
+	text.setFillColor(sf::Color::White); // Couleur par défaut
+	m_RenderWindow = I(GameManager)->GetWindow();
+}
+
+TextComponent::~TextComponent() 
+{
+}
+
+void TextComponent::SetText(const std::string& content, unsigned int fontSize, const sf::Vector2f& position) 
+{
+	text.setString(content);
+	text.setCharacterSize(fontSize);
+	text.setPosition(position);
+}
+
+void TextComponent::SetColor(const sf::Color& color) 
+{
+	text.setFillColor(color);
+}
+
+void TextComponent::Draw() {
+	if (m_RenderWindow) {
+		m_RenderWindow->draw(text);
+	}
+}
+
+
--- Template-Breakout/Components/TextComponent.h
+++ Template-Breakout/Components/TextComponent.h
@@ -1,8 +1,24 @@
 #pragma once
 
 #include "Component.h"
+#include <SFML/Graphics.hpp>
 
-class TextComponent : public Component
+
+class TextComponent : public Component 
 {
-};
+public:
+	TextComponent();
+	~TextComponent();
 
+	void SetText(const std::string& content, unsigned int fontSize, const sf::Vector2f& position);
+
+	void SetColor(const sf::Color& color);
+
+	virtual void Draw() override;
+
+private:
+	sf::Font font;	
+	sf::Text text = sf::Text(font, "hello");
+
+	sf::RenderWindow* m_RenderWindow = nullptr; // Référence à la fenêtre de rendu SFML
+};
